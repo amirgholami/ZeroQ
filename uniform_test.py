2d85060c3756d7c4380a0abfacc38b8f6713d812
Zhen-Dong <zhendong@berkeley.edu>
2020-03-25 22:54:02 -0700

    load&save added

--- uniform_test.py
+++ uniform_test.py
@@ -30,7 +30,9 @@ from distill_data import *
 # model settings
 def arg_parse():
     parser = argparse.ArgumentParser(
-        description='This repository contains the PyTorch implementation for the paper ZeroQ: A Novel Zero-Shot Quantization Framework.')
+        description=
+        'This repository contains the PyTorch implementation for the paper ZeroQ: A Novel Zero-Shot Quantization Framework.'
+    )
     parser.add_argument('--dataset',
                         type=str,
                         default='imagenet',
@@ -53,6 +55,14 @@ def arg_parse():
                         type=int,
                         default=128,
                         help='batch size of test data')
+    parser.add_argument('--save',
+                        type=str,
+                        default=None,
+                        help='save the quantized model')
+    parser.add_argument('--load',
+                        type=str,
+                        default=None,
+                        help='load a quantized model')
     args = parser.parse_args()
     return args
 
@@ -62,32 +72,39 @@ if __name__ == '__main__':
     torch.backends.cudnn.deterministic = False
     torch.backends.cudnn.benchmark = True
 
-    # Load pretrained model
-    model = ptcv_get_model(args.model, pretrained=True)
-    print('****** Full precision model loaded ******')
+    # Load a pretrained model or a quantized model
+    if args.load is not None:
+        model = ptcv_get_model(args.model, pretrained=False)
+        model.load_state_dict(torch.load(args.load))
+        quantized_model = model.eval()
+        print('****** Quantized model loaded ******')
+    else:
+        model = ptcv_get_model(args.model, pretrained=True)
+        print('****** Full precision model loaded ******')
+
+        # Generate distilled data
+        dataloader = getDistilData(
+            model.cuda(),
+            args.dataset,
+            batch_size=args.batch_size,
+            for_inception=args.model.startswith('inception'))
+        print('****** Data loaded ******')
+
+        # Quantize single-precision model to 8-bit model
+        quantized_model = quantize_model(model)
+        # Freeze BatchNorm statistics
+        quantized_model.eval()
+        quantized_model = quantized_model.cuda()
+
+        # Update activation range according to distilled data
+        update(quantized_model, dataloader)
+        print('****** Zero Shot Quantization Finished ******')
 
     # Load validation data
     test_loader = getTestData(args.dataset,
                               batch_size=args.test_batch_size,
                               path='./data/imagenet/',
                               for_inception=args.model.startswith('inception'))
-    # Generate distilled data
-    dataloader = getDistilData(
-        model.cuda(),
-        args.dataset,
-        batch_size=args.batch_size,
-        for_inception=args.model.startswith('inception'))
-    print('****** Data loaded ******')
-
-    # Quantize single-precision model to 8-bit model
-    quantized_model = quantize_model(model)
-    # Freeze BatchNorm statistics
-    quantized_model.eval()
-    quantized_model = quantized_model.cuda()
-
-    # Update activation range according to distilled data
-    update(quantized_model, dataloader)
-    print('****** Zero Shot Quantization Finished ******')
 
     # Freeze activation range during test
     freeze_model(quantized_model)
@@ -95,3 +112,7 @@ if __name__ == '__main__':
 
     # Test the final quantized model
     test(quantized_model, test_loader)
+
+    # Save the final model
+    if args.save is not None:
+        torch.save(model.state_dict(), args.save)
